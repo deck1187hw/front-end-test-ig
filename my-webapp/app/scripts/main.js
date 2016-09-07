4958c9f93f88684d24141a8ef98b1a7dc94b75fe
puiggm <miguel.puig@ig.com>
2016-09-07 14:12:04 +0100

    initial commit

---
+++ my-webapp/app/scripts/main.js
@@ -0,0 +1,39 @@
+
+/*************** Exercise 1 *****************/
+/* Quiz Javascript and CSS and write your score below */
+/* http://davidshariff.com/quiz/  */
+/* Your SCORE: XXX */
+
+
+
+
+
+/*************** Exercise 2 *****************/
+/* Inside customModule, create a method called numVowels() that receives a string and returns the number of vowels contained on the string.
+You should call the module function externally and console log the result. 
+Example:
+var numVowels = myApplication.customModule.numvowels("this is a phrase lets count the vowels on it"); 
+console.log(numVowels); // returns 13
+ */
+
+
+
+var myApplication = myApplication || {};
+
+myApplication.customModule = (function() {  
+
+   
+})();
+
+// Num Vowels goes here
+
+
+
+
+
+
+/*************** Exercise 3 *****************/
+/* Code in CSS / HTML the image supplied
+When finished with the design add some interaction and when the user clicks the input, do an jQuery AJAX POST request to http://jsonplaceholder.typicode.com/posts
+and send the value of the input box stored in an object like {inputValue:'',yourname:'test'} and console log the output
+*/
